70f08b1f04641dc5f9a65e0d1182b33b6a362684
Maarten Nieber <hallomaarten@yahoo.com>
2021-07-14 08:59:47 +0200

    Fix router config of select-item-effect

--- moonleap_react_state/selectitemeffect/props.py
+++ moonleap_react_state/selectitemeffect/props.py
@@ -1,5 +1,6 @@
+from moonleap_react_view.router import RouterConfig
 from moonleap_react_view.router_and_module.props import create_component_router_config
 
 
 def create_router_configs(self):
-    return [create_component_router_config(self, url=f":{self.item_name}Id")]
+    return [RouterConfig(component=self, url="")]
