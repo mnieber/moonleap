d720bd5731b63137d343c26a2ab0c21c5fd2d452
Maarten Nieber <hallomaarten@yahoo.com>
2021-07-14 08:15:04 +0200

    Set url of listview

--- moonleap_react_state/listview/props.py
+++ moonleap_react_state/listview/props.py
@@ -1,11 +1,13 @@
+from moonleap_react.component.resources import get_component_base_url
 from moonleap_react_view.router.resources import prepend_router_configs
 from moonleap_react_view.router_and_module.props import create_component_router_config
 
 
 def create_router_configs(self):
-    router_configs = [create_component_router_config(self)]
+    router_config = create_component_router_config(self)
+    router_config.url = get_component_base_url(self, self.item_name)
     result = prepend_router_configs(
-        self.load_items_effect.create_router_configs(), router_configs
+        self.load_items_effect.create_router_configs(), [router_config]
     )
 
     for state in self.module.states:
